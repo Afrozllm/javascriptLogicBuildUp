d5fc6290fd05c97aa2de72eb4fffbbca743bb79d
mdafroz3957@gmail.com <mdafrozalam109@gmail.com>
2026-02-10 10:22:34 +0530

    some add logic

--- First/firstjs.js
+++ First/firstjs.js
@@ -1,2 +1,14 @@
-console.log("Helloworld");
- var str ="hellow"
+
+// let A=100;
+// let B="200"
+
+// if(A==B || B==A){
+//     console.log("Hellow everyone welcome to js1");
+// }else{
+//     console.log("Hellow everyone welcome to js2")
+// }
+// console.log(!false);//agar single not ! used karte ho to hame ye true dega  but agar ham Double ! ka used karte hai to  false ka  dega 
+// console.log(!false);
+// console.log(!true);
+// console.log(!false);;//
+
