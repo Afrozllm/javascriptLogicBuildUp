b4a80ed21ab5c724e9fd8b8af87461c9fa8acad2
mdafroz3957@gmail.com <mdafrozalam109@gmail.com>
2026-02-13 18:35:16 +0530

    variable and data type learning

---
+++ Third/script.js
@@ -0,0 +1,80 @@
+// 1. Variables & Data Types
+//variable create karne ka 3 tarika hai //
+// var name="Mizan";
+// let age=30;
+// const discount=40%;
+
+// var city ="Delhi";
+// console.log(city)
+// var city="Mumbai";
+// console.log(city)
+
+
+// function test(){
+//     let y=100;
+//     if(true){
+//         let x=10;
+//         console.log(x)
+//         console.log(y)
+//     }
+    
+    
+// }
+// // console.log(x)
+// let final=test()
+// / var a=100;
+// console.log(a);
+//  var a=200;
+//  var a=300;
+//  var a =800
+
+// console.log(a)
+//ham variable se declear karte hai variable too ham isko fir se re-declear kar sakte hain and fir se declear kar sakte hai //
+// let a=100;
+// console.log(a)//100//
+// let a=100; taab muje ye error dega aur error hai can not be re-declear let variable
+// let a=100; already has beeen decleared 
+// a=200;
+
+// let num=100;
+
+// console.log(num)//100//
+
+// let num=100;
+
+// console.log(num);//error dega bolega ke num jo hai already decrear hai but agar main srif num=100 tab muje error nahi dega
+// num=300;
+// console.log(num)main me num ko upgrade kardia let variable me .
+// jaab v ham let se koyi v variable baanate hai tab ham usko update kar sakte hai but re-declear nahi kar sakte//
+// re-declear KA MATLAB MUJEE SAMAJ ME ATA HAI KE AGAR MAI 
+//LET A=100;
+// LET A=200;//JO KE ERROR dega
+// HUM YE CHEECH VAR VARIABLE SE KARSAKTE HAIN//AUR KOYI V ERROR NAHI DEGA BUT HUM VAR KO AVOID KARTE HAI //
+//AVI TWO MORDER VARIABLE CREATE KARNE KA KEY WORDS HAI JAVASCRIPTS ME 
+//LET ,CONST.
+// CONST VARIABLE SE HUM VARIAVLE DECLEAR KARTE HAI TOO FIR ISKO DOBARA NAHI UPDATE KARSAKTE HAI AUR NA CHANGE //YE FOIXED HOTE HAIN/EXAMPLE 
+// const discount=40% //ye fixed hai 
+
+
+// SCOPE//
+// {
+//     let a=10;
+//     console.log(a);
+// }
+// console.log(a)//error
+
+//function scope//
+// function ABC(){//outer function hai//
+// let age=24;
+// let name="rahul"
+// // const marks=59;
+// //     // inner function//
+// //     function abc(age,name,marks){
+// //         console.log(`${age}, ${name},${marks}`);
+        
+// //     }
+// //     return abc(age,name,marks);
+// // }
+
+// // let Outfn=ABC();
+// // console.log(Outfn);
