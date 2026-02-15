a22bab2a96532a8942677aec2f36b375eb75e505
mdafroz3957@gmail.com <mdafrozalam109@gmail.com>
2026-02-15 19:04:36 +0530

    revise_sunday 15/02/2026

---
+++ Fsunday/revision.js
@@ -0,0 +1,98 @@
+// Online Javascript Editor for free
+// Revision//
+//Logical operators//
+// &&,|| !;
+// console.log("afroz" && "0");//falsey value return karta hai first// 0 output ayega 
+// console.log("false" && 0);// 0 ayega
+// console.log(false && true);//false
+// console.log(false && false); //false ayega q ke first he me inko falsy value milgaya=false
+// console.log(1 && 0); //false he ayega matlab 0 
+// console.log(0 && 0);//zero ayega 
+// console.log("world" && true);//true ayega first falsy value ko dekhta hai agar falsy value mila to result fasly ayega but agar nahi mila to next jo hai right mega its wo true ho ya false wahi return karega//=true//
+// console.log(-10 && "0");//0 melega;
+// console.log(-100 && false);//false melaga
+// truthy and falsy value ke according output araha hai so keep your mind
+// let num = 20;//truethy value hai
+// if(num){
+//     console.log("world"); // ye ayega
+// }else{
+//     console.log("laptop");
+// }
+
+// let fail=false;
+// if(fail){
+//     console.log("You are passed")
+// }else{
+//     console.log("You are failed")//ye ayega q ke initial falsy value hai //
+// }
+// let a=100;//truthy value hai
+// let b="100";//truthy value hai
+//dono ke dono condition sahi hai//
+// if(a && b){
+//     console.log("checkedIn...............")
+// }else{
+//     console.log("Checkout.............")
+// }
+
+//yaha pe jo compare ho arah hai truthy aur falsy  based pe//
+//finally output ayega  checkedIn
+// let a=100;
+// if(a){
+//       console.log("checkedIn...............")
+// }else{
+//     console.log("Checkout......................")
+// }
+
+// let a=true;
+// let b=false;
+// console.log(a && b);//
+
+// let b=0;
+// let c=1;
+// console.log(b && c);
+
+// let b="world";
+// let c="0";
+// console.log(b && c);
+ 
+//  let letter= "A";
+//  let word="apple";
+//  if(letter && word){
+//     console.log("say")
+
+//  }else{
+//     console.log("Hii")
+//  }
+
+// let a=0;
+// let b="123";
+// if(a && b){
+//     console.log("Mai hu truthy value")
+// }else{
+//     console.log("Mai too hu bhai falsy value!")
+// }
+
+// console.log(0 && true);//falsey value hai 0; aur && AND operator first falsy value ko dekhta hai aur next ko check he nahi kya direct output//
+
+// let str="world";
+// let num=1000;
+// console.log(str && num);//1000;
+// let ab="abcd";
+
+// let nb=Number(ab);
+// console.log(nb);
+// console.log(typeof(nb));
+
+// let str="hello";
+
+// // let cntnum=Number(str);
+// // console.log(cntnum);
+
+// let cntnum= isNaN(str)
+// console.log(cntnum)
+
+// if(cntnum){
+//     console.log("Mobile")
+// }else{
+//     console.log("iphone17-Pro")
+// }
--- Third/script.js
+++ Third/script.js
@@ -78,3 +78,431 @@
 
 // // let Outfn=ABC();
 // // console.log(Outfn);
+// ......................//////////////////////////////..........................................................
+// your code goes here
+
+// const obj={
+//     name:"afroz"
+    
+// }
+
+// let ans=obj.age;
+// console.log(ans);
+// const obj = {
+//     name: "afroz"
+// }
+
+// let ans = obj.age;
+// console.log(ans); // undefined
+
+// Kyun undefined aata hai?
+// Kyunki age property obj mein exist hi nahi karti।
+// JavaScript mein jab tum kisi object ki aisi property access karte ho jo exist nahi karti, toh JS error nahi deta — balki undefined return karta hai।
+// hum const se variable create kaerte hai to ham isko change nahi kar sakte na q ke ye fixed hote hai BUT Ham ek cheez kar sakte hai wo hai aur wo hai ke property ko change kar sakte hai Array and Objects ke .
+// example
+
+// let persion={name:"rahul",Age:24};
+// console.log(persion)
+
+//  persion.name ="Asif";
+//  console.log(persion)
+// ham ko pata hai ke const se hum variable create karte hai to ham change nahi kar sakte hai but kuch cheez Kar sakte hain jaise hum jo object banayenege const se too uske ander jo v property hai sab change kar sakte 
+
+// 5. Scope — Bahut Important Concept;
+
+// var name ="asif";
+// let age=25;
+// const marks=80;
+
+// function abc(a){
+//     console.log(name);
+//     console.log(age);
+//     console.log(marks)
+    
+// // }
+// // let Ans=abc("300")
+// // console.log(Ans);
+
+// function test(){
+//     var a=100;
+//     console.log(a);
+//     function abcd(){
+        
+//         console.log(a)
+//     }
+//     return abcd()
+
+// }
+// // console.log(a);
+
+// let abc=test();
+
+
+
+
+
+
+// 6. Hoisting — Variable Upar Uth Jaata Hai//
+
+
+// console.log(a);
+// var a=10;
+
+// let a=10;
+// Cannot access 'a' before initialization
+// temporal dead zone me rahta hai jaab hum let aur const variable //
+
+// console.log(a);
+// function abc(){
+//     console.log("hellow")
+// }
+
+// let a=abc();// ye v temporal dead zone me rahta hain
+// // console.log(a);
+
+
+// console.log(test())//undefine milta hai too 2 reason hai 
+// first hai ke hum function test() me ander kuch nahi likhe hai aur second hai ke hum test() value nahi like kuch
+
+// function test(){
+    
+// }
+
+// function say(){
+//     return "afroz"
+// }
+// console.log(say())
+
+// number ko return karna hai //
+
+// function say(){
+//     let a=100;
+//     let b=200;
+//     return a+b;
+// }
+// console.log(say());
+
+
+// function abc(){
+//     return {
+//         Name:"asif",
+//         Age:30,
+//         State:"west bengal",
+//         Live: "Siliguri"
+//     }
+// }
+
+// console.log(abc())
+// function sum(a,b){
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+// let total=sum(10,20);
+// console.log(total)
+
+// ......................///////////////////////////..................................
+// console.log(10/0)
+// console.log(20 * "a")
+
+// let num=3.14159;
+// let final=num.toFixed(2);
+// console.log(final);
+
+// let firstnum=234567;
+
+// let bb=firstnum.toString()
+// console.log(firstnum);
+// // console.log(typeof(bb))
+
+// let str="123546";
+
+// let Strchange=Number(str);
+// console.log(Strchange);
+// console.log(typeof(Strchange));
+
+
+// let a=true;//1
+
+// let b=Number(a);
+// console.log(b);//1
+// console.log(typeof(b));
+
+// let c=false;
+// let d=Number(c);
+// console.log(d)
+
+// console.log(typeof(d))
+
+
+// let ab=null;
+
+// let bd=Number(ab);
+// console.log(bd);
+// console.log(typeof(bd))
+// .....////////////////////..........................
+
+
+
+// console.log(Number("hellow"));
+
+// console.log(String(123));
+// console.log(typeof(String))
+
+
+
+
+// console.log(isNaN(123));//false;
+// console.log(isNaN("Afroz"));//true;
+// console.log(isNaN(true)); //false;
+// console.log(isNaN(false))//false;
+// console.log(isNaN(Object));//false;
+// console.log(isNaN(null));//false
+// console.log(isNaN([]))//false
+// console.log(isNaN({})) true
+
+// console.log("12" * 2);
+
+// console.log(isNaN(12 * "a"))/////NaN//
+
+// console.log("hlo"/2 );//NaN//
+// console.log("abc" * 2)//NaN;;
+// console.log("abc" + 2)//abc2//
+// console.log("abc" * "abc")//
+
+
+
+// let str="hellow";
+
+// let str1=Number(str);
+// console.log(str1);
+
+// let num=Number(undefined)
+// console.log(num)
+
+// let ft="2334.746";
+// console.log(Number(ft));
+
+// let x;
+// console.log( x * "2");
+// console.log(x)
+// not a number//
+// console.log("abc" * "abc")
+// console.log("abc" -123)
+// console.log("123" / "abc")
+
+
+// ../////////......
+
+//1. let A ="say";
+
+// let b=Number(A);;;
+// console.log(b)NaN//
+
+2.
+// let abc="123";
+// let finaln=Number(abc);
+// console.log(finaln)//Number dega q ke ye ek number //
+// console.log(typeof(finaln))
+
+
+
+
+// parseFloat and parseInt//agar hum ye dono ke parameter me pass karte hai string to hame NaN dega//
+
+// let abc="laptop";
+// let finalb=parseFloat(abc);
+// console.log(finalb);//NaN// dega
+
+// let phone="Iphone";
+
+// let finalc=parseInt(phone);
+// console.log(finalc);//NaN//
+
+
+
+
+// let num =123;
+// let num1=parseFloat(num);
+// console.log(num1);//Number dega q ke ye ek number hain
+// console.log(typeof(num1));
+
+
+
+// let num="1234";
+// let num1=parseInt(num);
+// console.log(num1);//Number dega q ke ye ek number agar hum type check kare too//
+
+// console.log(typeof(num1))
+// console.log(num);
+
+
+// MATH//
+// let bignum=-8642;;
+
+// let finalnum=Math.sqrt(bignum);
+// console.log(finalnum)
+
+
+
+// NaN ki khasiyan hai//
+
+// console.log(NaN === NaN);//false//
+
+// if(NaN === NaN){
+//   console.log("Yes i am true")
+// }else{
+//   console.log("i am false");//false result dega q ke NaN khud se v compare nahi karta hai;
+// }
+
+// if(isNaN === isNaN){
+//   console.log("world");//world ayega q ke isNaN equal hai isNaN ke 
+// }else{
+//   console.log("Hiii!")
+// }
+//hum ko NaN ko nahi used kare to accha hoga q ye khavi khavi true false deta hai aur khavi khavi false ko true//
+//hum ko isNaN used karna chaiye real time //
+
+
+// let name="afroz" - 10;
+// console.log(name);
+
+// // console.log(NaN(name))
+// // console.log(name)
+// console.log("afr" * 2);
+
+// // console.log(isNaN(name))
+// let w=isNaN(name);
+// console.log(w);
+
+// if(w === true){
+//   console.log("haa bhai too pass ho gaya")
+// }else{
+//   console.log("Bhai ! too fail ho gaya  next time pass ho jiaga!");
+// }
+
+// // 
+// let inputUser="afroz";
+
+// // let randam=Number(inputUser);//false dene ke karan muje else part output milta hai
+// let randam=isNaN(inputUser)//true yaha true ke karan muje if wala part chalta hai//output//
+// console.log(randam)
+
+// if(randam === true){
+//   console.log("hellow sir are you pass")
+// }else{
+//   console.log("sorry sir! you are faile");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
