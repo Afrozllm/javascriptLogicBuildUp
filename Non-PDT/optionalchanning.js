5f78b9eb0cbb38339a3c3f9fb745d6e8b8ba138d
mdafroz3957@gmail.com <mdafrozalam109@gmail.com>
2026-03-08 01:07:29 +0530

    logic bulding if else and nested if else statement

---
+++ Non-PDT/optionalchanning.js
@@ -0,0 +1,28 @@
+// let usre = {
+//     fun:function(){
+//         console.log("This is my function objects");
+
+//     }
+// }
+
+// usre.fun();
+// console.log(usre?.fun());
+// // usre?.buy();
+
+// let usre1= {
+//     name:"afroz",
+// }
+// console.log(usre1?.name);
+// console.log(usre1?.age);
+
+let abc={
+    name:"xyz"
+}
+console.log(abc.name);
+
+
+
+
+console.log(abc?.name)
+console.log(abc?.age);
+console.log(abc?. name?.firtsname)
