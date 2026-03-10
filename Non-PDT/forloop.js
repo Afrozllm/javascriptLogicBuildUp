bdb8dc6c58cdf8f8ac2c2a5dcd9ee6d1beb7a0d0
mdafroz3957@gmail.com <mdafrozalam109@gmail.com>
2026-03-10 13:02:37 +0530

    looping concept done 10 march

---
+++ Non-PDT/forloop.js
@@ -0,0 +1,18 @@
+// for(let i=0 ;i<=5; i++){
+//     console.log(i)
+
+// }
+console.log( 5 >= 0);//true
+
+//reverse for loop//
+// for(let i=5; i>=0; i--){
+//     console.log(i)
+// }
+
+
+// console.log( 0 >=10);//false
+// let num=5;
+// for(let j=0; j<=10; j++){
+//     console.log(j);
+//     console.assert.log(num * j)
+// }
